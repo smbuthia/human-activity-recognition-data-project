12008c07508f608127f7effc41f556ac1b32baf6
smbuthia <skmbuthia@gmail.com>
2016-07-27 14:37:27 +0300

    Added CodeBook.md and README.md. Added new functions to run_analysis.R.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,14 @@
-mergeall <- function(){
+## merges the training and test data sets to create one data set
+mergeall <- function() {
+    
+}
+
+## extracts only the measurements on the mean
+getmeanmeasurements <- function() {
+    
+}
+
+## extracts only the measurements on the standard deviation
+getsdmeasurements <- function() {
     
 }
